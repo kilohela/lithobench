d8574d93f4c2c2da741f3611e2b08063fef737bc
hela <kilohela@163.com>
2025-12-01 18:05:26 +0800

    subtle changes

--- mine/unet_backbone.py
+++ mine/unet_backbone.py
@@ -254,7 +254,7 @@ class UnetBackbone(ModelILT):
                 
                 # Forward pass
                 with autocast(self.device.type):
-                    printedNom, printedMax, printedMin = self.simLitho(F.sigmoid(self.nn(target)).squeeze(1)).squeeze(1)
+                    printedNom, printedMax, printedMin = self.simLitho(F.sigmoid(self.nn(target)).squeeze(1).squeeze(1))
                     loss_l2 = F.mse_loss(printedNom, target)
                     loss_pvb = F.mse_loss(printedMax, printedMin)
                     loss = loss_l2 + loss_pvb
