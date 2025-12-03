4e1b05e41a0cf9f71edeaad8f5d2d29a90841a41
hela <kilohela@163.com>
2025-12-03 23:20:17 +0800

    subtle changes

--- mine/plot_loss.py
+++ mine/plot_loss.py
@@ -3,7 +3,14 @@ import torch
 import argparse
 import os
 
-def plot_loss_curve(checkpoint_path, output_path):
+'''
+This script is used to plot the loss curves from a training checkpoint. 
+It reads the checkpoint file and extracts the training and validation loss values. 
+It then plots these values on a graph and saves the graph to a file.
+An optional baseline loss curve can also be plotted for comparison.
+'''
+
+def plot_loss_curve(checkpoint_path, baseline_path, output_path):
     """
     Reads a training checkpoint and plots the training and validation loss curves.
 
@@ -14,6 +21,7 @@ def plot_loss_curve(checkpoint_path, output_path):
     try:
         checkpoint = torch.load(checkpoint_path)
         logger = checkpoint['logger']
+        baseline_logger = torch.load(baseline_path)['logger'] if baseline_path is not None else None
     except FileNotFoundError:
         print(f"Error: Checkpoint file not found at '{checkpoint_path}'")
         return
@@ -24,6 +32,9 @@ def plot_loss_curve(checkpoint_path, output_path):
     plt.figure(figsize=(10, 5))
     plt.plot(range(1, num_steps + 1), logger['train_loss'], label='Train Loss')
     plt.plot(range(val_steps_interval, num_steps + 1, val_steps_interval), logger['val_loss'], label='Validation Loss')
+    if baseline_logger is not None:
+        plt.plot(range(1, len(baseline_logger['train_loss']) + 1), baseline_logger['train_loss'], label='Baseline Train Loss')
+        plt.plot(range(baseline_logger['val_steps_interval'], len(baseline_logger['val_loss']) + 1, baseline_logger['val_steps_interval']), baseline_logger['val_loss'], label='Baseline Validation Loss')
     plt.xlabel('Steps')
     plt.ylabel('Loss')
     plt.title(f'Loss Curve of {os.path.basename(checkpoint_path)}')
@@ -35,9 +46,12 @@ def plot_loss_curve(checkpoint_path, output_path):
 def parseArgs(): 
     parser = argparse.ArgumentParser(description="Read a checkpoint file and plot the loss curve.")
     parser.add_argument("--checkpoint", "-c", required=True, type=str, help="Path to the checkpoint file")
+    parser.add_argument("--output", "-o", required=False, type=str, help="Path to save the plot image")
+    parser.add_argument("--baseline", "-b", required=False, type=str, help="baseline checkpoint path")
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parseArgs()
-    output_path = os.path.join('mine', 'outputs', f'{os.path.basename(args.checkpoint)}_loss.png')
-    plot_loss_curve(args.checkpoint, output_path)
+    baseline_path = args.baseline
+    output_path = os.path.join('mine', 'outputs', f'{os.path.basename(args.checkpoint)}_loss.png') if args.output is None else args.output
+    plot_loss_curve(args.checkpoint, args.baseline, output_path)
